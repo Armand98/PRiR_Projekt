14ddd7672dfb58cccf74e5f1d299cea16259f42a
Armand <pajorarmand@gmail.com>
2021-11-19 18:58:33 +0100

    Refaktor kodu i usunięcie zbędnych funkcji testowych.

--- PRiR_Projekt.cpp
+++ PRiR_Projekt.cpp
@@ -9,36 +9,15 @@
 #include <omp.h>
 #include <vector>
 #include <string>
+#include <time.h>
 
-std::vector<int> getSelectedNumbers(int lowerLimit, int upperLimit, int minimalDigitsSum)
+std::vector<int> generateRandomNumbersInRange(int minNumber, int maxNumber, int quantity)
 {
-    std::vector<int> selectedNumbers;
-    //double time = omp_get_wtime();
-
-    for (int i = lowerLimit; i <= upperLimit; i++)
-    {
-        std::string number = std::to_string(i);
-        int actualDigitsSum = 0;
-
-        for (char digit : number)
-            actualDigitsSum += digit - '0';
-
-        if (actualDigitsSum > minimalDigitsSum)
-            selectedNumbers.push_back(i);
-    }
-
-    //double measuredTime = omp_get_wtime() - time;
-    //std::cout << "Czas tworzenia zbioru danych: " << measuredTime << std::endl;
-
-    return selectedNumbers;
-}
-
-std::vector<int> inRange(int lowerLimit, int upperLimit)
-{
+    srand(time(NULL));
     std::vector<int> numbers;
 
-    for (int i = lowerLimit; i <= upperLimit; i++)
-        numbers.push_back(i);
+    for (int i = 1; i <= quantity; i++)
+        numbers.push_back(rand() % maxNumber + minNumber);
 
     return numbers;
 }
@@ -94,35 +73,39 @@ double getAveregeWithValidationParallel(std::vector<int> numbers, int lowerLimit
 
     double measuredTime = omp_get_wtime() - time;
 
-    std::cout << "Czas liczenia sredniej arytmetycznej z walidacja danych wejsciowych przy uzyciu " << threads << " watkow wynosi: " << measuredTime << std::endl;
+    if (threads > 1)
+        std::cout << "Czas liczenia sredniej arytmetycznej z walidacja danych wejsciowych przy uzyciu " << threads << " watkow wynosi: " << measuredTime << std::endl;
+    else
+        std::cout << "Czas liczenia sredniej arytmetycznej z walidacja danych wejsciowych przy uzyciu " << threads << " watka wynosi: " << measuredTime << std::endl;
 
     return (double)sum / (double)quantity;
 }
 
 int main()
 {
-    int lowerLimit = 0;
+    int lowerLimit = 1000;
     int upperLimit = 2000000;
     int minimalDigitsSum = 15;
 
+    int minimalValueInDataset = 1;
+    int maximalValueInDataset = 10000000;
+    int quantityOfDataset = 5000000;
+
     std::cout << "Trwa przygotowywanie danych... " << std::endl;
-    std::vector<int> dataset1 = getSelectedNumbers(lowerLimit, upperLimit, minimalDigitsSum);
-    std::vector<int> dataset2 = inRange(lowerLimit, upperLimit);
+    std::vector<int> dataset = generateRandomNumbersInRange(minimalValueInDataset, maximalValueInDataset, quantityOfDataset);
     std::cout << "Dane zostaly wygenerowane. Zaczynam obliczenia: " << std::endl << std::endl;
 
-    std::cout << "(1) Srednia arytmetyczna bez walidacji: " << getAverege(dataset1) << std::endl;
+    std::cout << "(1) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(2) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum) << std::endl;
+    std::cout << "(2) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 2) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(3) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 2) << std::endl;
+    std::cout << "(3) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 4) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(4) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 4) << std::endl;
+    std::cout << "(4) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 6) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(5) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 6) << std::endl;
+    std::cout << "(5) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 8) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(6) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 8) << std::endl;
+    std::cout << "(6) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 10) << std::endl;
     std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(7) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 10) << std::endl;
-    std::cout << "--------------------------------------------------------------------------------" << std::endl;
-    std::cout << "(8) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset2, lowerLimit, upperLimit, minimalDigitsSum, 12) << std::endl;
+    std::cout << "(7) (Parallel) Srednia arytmetyczna z walidacja: " << getAveregeWithValidationParallel(dataset, lowerLimit, upperLimit, minimalDigitsSum, 12) << std::endl;
 }
